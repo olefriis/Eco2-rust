149cbeac69c0861ab38ce461eb96f04f9bfc62ca
Ole Friis Østergaard <olefriis@gmail.com>
2021-04-11 10:15:31 +0200

    Add list command (#9)

---
+++ src/commands/list.rs
@@ -0,0 +1,17 @@
+use crate::models::thermostats::Thermostats;
+use crate::models::parsed_thermostat::ParsedThermostat;
+
+pub fn execute(arguments: Vec<String>) {
+    if arguments.len() != 0 {
+        panic!("Expected no  parameters to list. Got {}.", arguments.len());
+    }
+
+    let thermostats = Thermostats::load().expect("Could not read thermostat data");
+    let mut thermostats = thermostats.thermostats;
+    thermostats.sort_by(|t1, t2| t1.name.cmp(&t2.name));
+
+    for thermostat in thermostats {
+        let parsed_thermostat = ParsedThermostat::from_thermostat(&thermostat);
+        println!("{} {}", thermostat.serial, parsed_thermostat.name);
+    }
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,3 +1,4 @@
+pub mod list;
 pub mod read;
 pub mod scan;
 pub mod show;
@@ -6,8 +7,4 @@ pub mod sync;
 
 pub fn forget(arguments: Vec<String>) {
     panic!("Forgetting is not implemented yet")
-}
-
-pub fn list(arguments: Vec<String>) {
-    panic!("Listing is not implemented yet")
 }
